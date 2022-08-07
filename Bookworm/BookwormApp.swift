e7705c6521da03ee98ff74fea81c729fb7a5edf6
kashiash <kashiash@gmail.com>
2022-08-07 10:38:43 +0200

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Jacek Placek on 07/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
